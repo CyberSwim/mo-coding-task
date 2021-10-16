ff83b7fc34da3de9a20b73528c74c50c4fb9d0f5
[Aaron Donlon] <[aarondonlon@gmail.com]>
2021-10-16 19:01:19 +0100

    add unit tests and main program foundations

---
+++ Tests/tests.py
@@ -0,0 +1,20 @@
+import unittest
+import shield_calc as sc
+
+class TestMethods (unittest.TestCase):
+    
+    def test_number_separator(self):
+        self.assertEqual(sc.parse_numbers("1x2x3"), [1, 2, 3])
+        self.assertEqual(sc.parse_numbers("3441x5031x99001"), [3441, 5031, 99001])
+
+    def test_area_calc(self):
+        self.assertEqual(sc.area_calc([2, 5, 3]), 62)
+
+    def test_shield_contingency(self):
+        self.assertEqual(sc.shield_contingency_calc([10, 6, 2]), 12)
+
+    def test_wire_contingency(self):
+        self.assertEqual(sc.wire_contingency_calc([2, 8, 3]), 10)
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ shield_calc.py
@@ -0,0 +1,23 @@
+# import the input file as a list
+# separate the width/length/depth
+# convert to int
+# sort smallest -> largest
+# add to new list
+
+# calculate 2 * area of each
+# add together
+# calculate contingency for shielding and add to p1 answer
+# calculate contingency for wires and add to p2 answer
+
+
+def parse_numbers(input_string):
+    return
+
+def area_calc(dimensions):
+    return
+
+def shield_contingency_calc(dimensions):
+    return
+
+def wire_contingency_calc(dimensions):
+    return
